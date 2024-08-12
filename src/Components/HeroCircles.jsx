d22fa1c1ac215527b2ecb35346a302b6fd7e5414
Mohammad Ikram <ikramullah201812345@gmail.com>
2024-08-12 17:46:14 +0500

    Your commit message

--- src/Components/HeroCircles.jsx
+++ src/Components/HeroCircles.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Circle1 from '../assets/Images/Ellipse 4 (1).png'
 import Circle2 from '../assets/Images/Ellipse 5 (1).png'
 import Circle3 from '../assets/Images/Ellipse 6.png'
@@ -8,6 +8,25 @@ import Circle5 from '../assets/Images/Ellipse 8 (1).png'
 
 const HeroCircles = () => {
     const CircleData=[Circle1,Circle2,Circle3,Circle4,Circle5]
+    const [count, setCount] = useState(0); // Initial count set to 0
+    const maxCount = 1500;
+    useEffect(() => {
+      // This effect updates the counter every 1 millisecond
+      const intervalId = setInterval(() => {
+        setCount(prevCount => {
+          if (prevCount < maxCount) {
+            return prevCount + 3;
+          } else {
+            clearInterval(intervalId); // Stop the counter when reaching maxCount
+            return prevCount;
+          }
+        });
+      }, 1); // Update every 1 millisecond
+  
+      // Cleanup interval on component unmount
+      return () => clearInterval(intervalId);
+    }, []);
+  
   return (
     <div className='h-[49px] w-[302px] flex items-center justify-between  gap-3'>
 <div className='flex  '>
@@ -18,7 +37,7 @@ const HeroCircles = () => {
 ))}
 </div>
 <div className='w-[126px] h-[42px] flex items-center justify-center'>
-    <p className='text-[18px] text-nowrap font-roboto text-white' ><span className='font-bold' >1500+ <br /></span> 
+    <p className='text-[18px] text-nowrap font-roboto text-white' ><span className='font-bold' >{count}+ <br /></span> 
     satisfied Clients</p>
 </div>
             </div>
